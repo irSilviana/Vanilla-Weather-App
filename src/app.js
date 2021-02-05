839cd19b2d1d7d2f762aa970cc8eab4a9e9b1b5c
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-02-06 05:16:12 +0700

    Add showTime function
    update the JS, HTML and CSS that related with showing the time of the last updated data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
+function showTime(datetime) { 
+    let now = new Date(datetime);
+    let minute = now.getMinutes();
+    if (minute < 10) { 
+        minute = `0${minute}`;
+    } 
+    let hour = now.getHours();
+    let amPm = "AM";
+    if (hour > 12) {
+        amPm = "PM"
+    }
+    convertHour();
+
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[now.getDay()];
+    let date = now.getDate();
+    let months = ["Jan","Feb","Mar","Apr", "May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    let month = months[now.getMonth()];
+    let year = now.getFullYear();
+
+    document.querySelector("#clock").innerHTML = `${hour}:${minute}`;
+    document.querySelector("#day").innerHTML = day; 
+    document.querySelector("#date").innerHTML = `${date} ${month} ${year}`;
+    document.querySelector("#amPm").innerHTML = amPm;
+
+    function convertHour() {
+    if (hour <= 9) {
+      hour = `0${hour}`;
+    } else if (hour > 9 && hour <= 12) {
+      return hour;
+    } else {
+      hour = hour - 12;
+      hour = `0${hour}`;
+    }
+  }
+
+}
+
 function showTemperature(response) {  
     let city = response.data.name;
     let country = response.data.sys.country;
@@ -7,7 +45,8 @@ function showTemperature(response) {
     let wind = Math.round(response.data.wind.speed);
     let humidity = Math.round(response.data.main.humidity);
     let feelsLike = Math.round(response.data.main.feels_like);
- 
+    showTime(new Date(response.data.dt * 1000));
+
     document.querySelector("#temperature").innerHTML = temp;
     document.querySelector("#city").innerHTML = `${city}, ${country}`;
     document.querySelector("#description").innerHTML = description;
@@ -16,6 +55,7 @@ function showTemperature(response) {
     document.querySelector("#humidity").innerHTML = humidity;
     document.querySelector("#feels-like").innerHTML = feelsLike;
 }
+
 
 let city = "New York";
 let unit = "metric";
