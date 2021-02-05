879ed68d8e94d3837efa804236092cc55ff78d6a
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-02-06 02:44:43 +0700

    Showing the current temperature
    Create showTemperature Function based on city

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 function showTemperature(response) {  
-    console.log(response);
+    let city = response.data.name;
+    let country = response.data.sys.country;
+    let temp = Math.round(response.data.main.temp);
+    let description = response.data.weather[0].description;
+    let cloudines = Math.round(response.data.clouds.all);
+    let wind = Math.round(response.data.wind.speed);
+    let humidity = Math.round(response.data.main.humidity);
+    let feelsLike = Math.round(response.data.main.feels_like);
+ 
+    document.querySelector("#temperature").innerHTML = temp;
+    document.querySelector("#city").innerHTML = `${city}, ${country}`;
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#cloudines").innerHTML = cloudines;
+    document.querySelector("#wind").innerHTML = wind;
+    document.querySelector("#humidity").innerHTML = humidity;
+    document.querySelector("#feels-like").innerHTML = feelsLike;
 }
 
-let city = " New York";
+let city = "New York";
 let unit = "metric";
 let apiKey = "125089b53f00feddd6fbd602dc6cec7a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
 
-console.log(apiUrl);
 
 axios.get(apiUrl).then(showTemperature);
